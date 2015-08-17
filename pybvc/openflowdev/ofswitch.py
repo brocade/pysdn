f5df554ea05feb959b20d6d344045e0db3a6bc33
jeb@elbrys.com <jeb@elbrys.com>
2015-08-17 12:47:36 -0400

    Upgraded to version 1.3.2, fix some comments causing autodoc problems

--- pybvc/openflowdev/ofswitch.py
+++ pybvc/openflowdev/ofswitch.py
@@ -1,35 +1,35 @@
 
+# Copyright (c) 2015,  BROCADE COMMUNICATIONS SYSTEMS, INC
+
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+
 """
-Copyright (c) 2015,  BROCADE COMMUNICATIONS SYSTEMS, INC
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-contributors may be used to endorse or promote products derived from this
-software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-THE POSSIBILITY OF SUCH DAMAGE.
-
 
 @authors: Sergei Garbuzov
 @status: Development
@@ -2231,9 +2231,9 @@ class PushPBBHeaderAction(Action):
         The 'ethernet_type' is used as the Ethernet Type for the tag. Only
         Ethernet Type  0x88E7 should be used 
         PBB - Provider Backbone Bridges is an Ethernet data-plane technology
-              (also known as MAC-in-MAC) that involves encapsulating an
-              Ethernet datagram inside another one with new source and
-              destination addresses. '''
+        .     (also known as MAC-in-MAC) that involves encapsulating an
+        .     Ethernet datagram inside another one with new source and
+        .     destination addresses. '''
     #---------------------------------------------------------------------------
     # 
     #---------------------------------------------------------------------------
@@ -2254,9 +2254,9 @@ class PopPBBHeaderAction(Action):
     ''' Pop the outer-most PBB service instance header (I-TAG TCI)
         from the packet
         PBB - Provider Backbone Bridges is an Ethernet data-plane technology
-              (also known as MAC-in-MAC) that involves encapsulating an
-              Ethernet datagram inside another one with new source and
-              destination addresses. '''
+        .     (also known as MAC-in-MAC) that involves encapsulating an
+        .     Ethernet datagram inside another one with new source and
+        .     destination addresses. '''
     #---------------------------------------------------------------------------
     # 
     #---------------------------------------------------------------------------
@@ -2627,9 +2627,9 @@ class Match(object):
         
         ''' IP match fields:
             - Differentiated Service Code Point (DSCP). Part of the IPv4 ToS field or
-              the IPv6 Traffic Class field.
+            . the IPv6 Traffic Class field.
             - ECN bits of the IP header. Part of the IPv4 ToS field or
-              the IPv6 Traffic Class field 
+            . the IPv6 Traffic Class field 
             - IPv4 or IPv6 protocol number '''
         self.ip_match = None
         
