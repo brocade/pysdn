a6153a88d7788d90589a5862a3d31965104bd32d
Gary Berger <gberger@brocade.com>
2015-11-21 09:23:12 -0500

    bump version

--- pybvc/__init__.py
+++ pybvc/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'pybvc'
-__version__ = '1.3.3'
-__author__ = 'Sergei Garbuzov'
+__version__ = '1.3.6'
+__author__ = 'Brocade Communications'
 __license__ = 'BSD'
 __copyright__ = 'Brocade Communications'
